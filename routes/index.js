21f6ac419ea118f182637baeff54e76f7ee63f7b
SynthCyraX <SynthCyraX@gmail.com>
2023-11-10 11:25:29 +0100

    Added controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const comment_controller = require('../controllers/commentController');
+const post_controller = require('../controllers/postController');
+const user_controller = require('../controllers/userController');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
