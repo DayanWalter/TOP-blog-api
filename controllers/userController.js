ce00ea6013d12d59d005b0ac065ec96b5014bc4d
SynthCyraX <SynthCyraX@gmail.com>
2023-11-12 05:01:30 +0100

    user_delete/put/detail/list added

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,7 @@ exports.user_create_get = asyncHandler(async (req, res, next) => {
   res.json({ user: 'user_create_get' });
 });
 exports.user_create_post = asyncHandler(async (req, res, next) => {
+  // Validate and Sanitize input
   const result = validationResult(req);
 
   const user = new User({
@@ -28,17 +29,36 @@ exports.user_delete_get = asyncHandler(async (req, res, next) => {
   res.json({ user: 'user_delete_get' });
 });
 exports.user_delete_delete = asyncHandler(async (req, res, next) => {
+  await User.findOneAndDelete({ _id: req.body.userid });
+
   res.json({ user: 'user_delete_delete' });
 });
 exports.user_update_get = asyncHandler(async (req, res, next) => {
   res.json({ user: 'user_update_get' });
 });
 exports.user_update_put = asyncHandler(async (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    isAuthor: req.body.isAuthor,
+    _id: req.body.userid,
+  });
+  await User.findByIdAndUpdate(req.body.userid, user, {});
   res.json({ user: 'user_update_put' });
 });
 exports.user_detail = asyncHandler(async (req, res, next) => {
-  res.json({ user: 'user_detail' });
+  const user = await User.findById(req.params.id).exec();
+
+  if (user === null) {
+    // No results.
+    const err = new Error('User not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.json({ user });
 });
 exports.user_list = asyncHandler(async (req, res, next) => {
-  res.json({ user: 'user_list' });
+  const allUser = await User.find().exec();
+  res.json({ allUser });
 });
