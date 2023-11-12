98563cbd36e30fe6d6d520bf750b20b5773b6d55
SynthCyraX <SynthCyraX@gmail.com>
2023-11-12 06:34:14 +0100

    comment/post_delete/put/detail/list added

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -12,6 +12,7 @@ exports.comment_post = asyncHandler(async (req, res, next) => {
   const comment = new Comment({
     text: req.body.text,
     timestamp: new Date().toLocaleString('en-US'),
+    user: req.body.user,
   });
   if (result.isEmpty()) {
     await comment.save();
@@ -21,15 +22,35 @@ exports.comment_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.comment_delete = asyncHandler(async (req, res, next) => {
+  await Comment.findOneAndDelete({ _id: req.params.id });
+
   res.json({ comment: 'comment_delete_delete' });
 });
 
 exports.comment_put = asyncHandler(async (req, res, next) => {
+  const result = validationResult(req);
+
+  const comment = new Comment({
+    text: req.body.text,
+    user: req.body.user,
+    _id: req.params.id,
+  });
+  await Comment.findByIdAndUpdate(req.params.id, comment, {});
+
   res.json({ comment: 'comment_update_put' });
 });
 exports.comment_detail = asyncHandler(async (req, res, next) => {
-  res.json({ comment: 'comment_detail' });
+  const comment = await Comment.findById(req.params.id).populate('user').exec();
+  if (comment === null) {
+    // No results.
+    const err = new Error('Comment not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.json({ comment });
 });
 exports.comment_list = asyncHandler(async (req, res, next) => {
-  res.json({ comment: 'comment_list' });
+  const allComments = await Comment.find().populate('user').exec();
+
+  res.json({ allComments });
 });
--- controllers/postController.js
+++ controllers/postController.js
@@ -7,19 +7,53 @@ const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
 exports.post_post = asyncHandler(async (req, res, next) => {
-  res.json({ post: 'post_create_post' });
+  const result = validationResult(req);
+
+  const post = new Post({
+    title: req.body.title,
+    text: req.body.text,
+    timestamp: new Date().toLocaleString('en-US'),
+    user: req.body.user,
+    visible: req.body.visible,
+  });
+  if (result.isEmpty()) {
+    await post.save();
+  }
+
+  res.json({ post });
 });
 
 exports.post_delete = asyncHandler(async (req, res, next) => {
+  await Post.findOneAndDelete({ _id: req.params.id });
+
   res.json({ post: 'post_delete_delete' });
 });
 
 exports.post_put = asyncHandler(async (req, res, next) => {
-  res.json({ post: 'post_update_put' });
+  const result = validationResult(req);
+
+  const post = new Post({
+    title: req.body.title,
+    text: req.body.text,
+    visible: req.body.visible,
+    _id: req.params.id,
+  });
+  await Post.findByIdAndUpdate(req.params.id, post, {});
+
+  res.json({ post });
 });
 exports.post_detail = asyncHandler(async (req, res, next) => {
-  res.json({ post: 'post_detail' });
+  const post = await Post.findById(req.params.id).populate('user').exec();
+  if (post === null) {
+    // No results.
+    const err = new Error('Post not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.json({ post });
 });
 exports.post_list = asyncHandler(async (req, res, next) => {
-  res.json({ post: 'post_list' });
+  const allPosts = await Post.find().populate('user').exec();
+
+  res.json({ allPosts });
 });
