ad60ba74fd724e62eedde736e92418962f05eba9
SynthCyraX <SynthCyraX@gmail.com>
2023-11-14 06:39:16 +0100

    FIrst draft frontend post request login

--- blog-api-frontend/src/App.jsx
+++ blog-api-frontend/src/App.jsx
@@ -2,26 +2,65 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [daten, setDaten] = useState();
+  // const [daten, setDaten] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/api', {
-          mode: 'cors',
-        });
-        const data = await response.json();
-        setDaten(data);
-      } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-      }
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:3000/api', {
+  //         mode: 'cors',
+  //       });
+  //       const data = await response.json();
+  //       setDaten(data);
+  //     } catch (error) {
+  //       console.error('Fehler beim Abrufen der Daten:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // const formData = new FormData(event.target);
+    // console.log(formData);
+    const formData = {
+      username: 'Admin',
+      password: '123',
     };
 
-    fetchData();
-  }, []);
+    try {
+      const response = await fetch('http://localhost:3000/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json); // Hier ist die JSON-Antwort nach dem Einreichen des Formulars
+      } else {
+        console.error('Fehler beim Anmelden');
+      }
+    } catch (error) {
+      console.error('Fehler:', error);
+    }
+  };
+
   return (
     <>
-      <div>{console.log(daten)}</div>
+      <div>
+        <h1>Welcome to the Authors Page</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   );
 }
