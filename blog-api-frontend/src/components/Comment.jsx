97f9a64a3fe2ce5d5133a5925035598c88233d55
SynthCyraX <SynthCyraX@gmail.com>
2023-11-15 10:36:06 +0100

    Comments now possible from a higher component

--- blog-api-frontend/src/components/Comment.jsx
+++ blog-api-frontend/src/components/Comment.jsx
@@ -1,52 +1,6 @@
-import { useState } from 'react';
 import styles from './Comment.module.css';
-import { useLoaderData } from 'react-router-dom';
 
 export default function Comment({ text, user }) {
-  const loaderData = useLoaderData();
-  const postid = loaderData.postid;
-
-  // Save input from form
-  const [formData, setFormData] = useState({
-    user: '',
-    text: '',
-    post: '',
-  });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formData);
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/post/${postid}/comment/create`,
-        {
-          method: 'POST',
-          body: JSON.stringify(formData),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (response.ok) {
-        const json = await response.json();
-        console.log(json);
-      } else {
-        console.error('Failed to comment');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
     <>
       <div className={styles.comment}>
@@ -56,25 +10,6 @@ export default function Comment({ text, user }) {
       <br />
       <hr />
       <br />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="text"
-          name="text"
-          value={formData.text}
-          onChange={handleChange}
-          placeholder="Text"
-        />
-        <input
-          type="text"
-          id="user"
-          name="user"
-          value={formData.user}
-          onChange={handleChange}
-          placeholder="Name"
-        />
-        <button type="submit">Send</button>
-      </form>
     </>
   );
 }
--- blog-api-frontend/src/components/Comments.jsx
+++ blog-api-frontend/src/components/Comments.jsx
@@ -2,10 +2,53 @@ import DataFetch from './DataFetch';
 import styles from './Comments.module.css';
 
 import Comment from './Comment';
+import { useState } from 'react';
 export default function Comments({ postid }) {
   const { loading, data, error } = DataFetch(
     'http://localhost:3000/api/comments'
   );
+
+  // Save input from form
+  const [formData, setFormData] = useState({
+    user: '',
+    text: '',
+    post: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formData);
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/post/${postid}/comment/create`,
+        {
+          method: 'POST',
+          body: JSON.stringify(formData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+      } else {
+        console.error('Failed to comment');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <>
       {loading && <div>A moment please...</div>}
@@ -14,8 +57,8 @@ export default function Comments({ postid }) {
       )}
       {data && (
         <div className={styles.comments}>
+          <p>Comments:</p>
           <ul>
-            <p>Comments:</p>
             {data.allComments.map(
               ({ text, user, _id, post }) =>
                 post._id === postid && (
@@ -25,6 +68,26 @@ export default function Comments({ postid }) {
                 )
             )}
           </ul>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="text"
+              name="text"
+              value={formData.text}
+              onChange={handleChange}
+              placeholder="Text"
+            />
+            <input
+              type="text"
+              id="user"
+              name="user"
+              value={formData.user}
+              onChange={handleChange}
+              placeholder="Name"
+            />
+            <button type="submit">Send</button>
+          </form>
         </div>
       )}
     </>
